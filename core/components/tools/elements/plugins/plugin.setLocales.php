7e4c22c3f5305f506be65caef0e4d8c7d84b57f6
Romain Tripault <romain@meltingmedia.net>
2011-06-19 13:32:40 +0200

    Completely removed setLocales plugin

--- core/components/tools/elements/plugins/plugin.setLocales.php
+++
@@ -1,7 +0,0 @@
-<?php
-if($modx->context->get('key') != "mgr") {
-    // @TODO: make use of the "category" property + allow language selection
-    setlocale(LC_ALL, 'fr_FR.' . $modx->getOption('modx_charset'));
-} else {
-    // we are in the manager, do not change locales
-}
