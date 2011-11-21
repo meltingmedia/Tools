0852967ce7e6c931f8e190cd95fde37b142f4594
Romain <romain@melting-media.com>
2011-11-21 13:22:02 +0100

    Slight update

--- core/components/tools/elements/snippets/hook.profilephoto.php
+++ core/components/tools/elements/snippets/hook.profilephoto.php
@@ -11,38 +11,41 @@
  */
 $user = $modx->user;
 $profile = $user->getOne('Profile');
-$target = $scriptProperties['target'];
-$photoUrl = $scriptProperties['photoUrl'];
-$picture = $hook->getValue($scriptProperties['photoField']);
+$path = $scriptProperties['path'];
+$path_url = $scriptProperties['path_url'];
+$field = $scriptProperties['field'];
+$fieldValue = $hook->getValue($field);
 
-if (empty($target) || empty($photoUrl) || empty($scriptProperties['photoField'])) {
-    $configError = 'target, photoUrl and/or photoField properties not defined (all are required).';
+if (empty($path) || empty($path_url) || empty($field)) {
+    $configError = 'path, path_url and/or field properties not defined (all are required).';
     //$hook->addError('message', $configError);
     $modx->log(modX::LOG_LEVEL_ERROR, $configError);
     return false;
 }
 
-if(!empty($picture['name'])) {
+if(!empty($fieldValue['name'])) {
     /**
      * array $photo
      * [name], [type], [tmp_name], [error], [size]
      */
     // @TODO: define some allowed file extensions
-    $ext = pathinfo($picture['name'], PATHINFO_EXTENSION);
+    $ext = pathinfo($fieldValue['name'], PATHINFO_EXTENSION);
     $filename = $user->get('username').'-profile.'.$ext;
     // @TODO: make sure the filename is unique & "web URL" compatible
-    $targetPhoto = $target.$filename;
+    $full_path = $path.$filename;
+    $full_url = $path_url.$filename;
 
-    if (!move_uploaded_file($picture['tmp_name'], $targetPhoto)) {
+    if (!move_uploaded_file($fieldValue['tmp_name'], $full_path)) {
         //$hook->addError($picture, 'File not uploaded');
         return false;
     }
 
     // Everything went fine, let's define the picture URL
-    $profile->set('photo', $photoUrl.$filename);
+    $profile->set('photo', $full_url);
     $profile->save();
-    //$hook->addError($picture, 'Everything ok');
 
 }
+
+// @TODO: allow a way to remove the profile picture
 
 return true;
