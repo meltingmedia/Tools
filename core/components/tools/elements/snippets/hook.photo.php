985b85777c4eedd5b9a326bb7105ce4a6f5cdfa2
Romain <romain@melting-media.com>
2011-11-21 14:06:31 +0100

    Slight update

--- core/components/tools/elements/snippets/hook.photo.php
+++ core/components/tools/elements/snippets/hook.photo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ProfilePhoto
+ * photo
  *
  * A Login.UpdateProfile post hook to allow users to upload a photo
  * for their profile
@@ -11,15 +11,26 @@
  */
 $user = $modx->user;
 $profile = $user->getOne('Profile');
-$path = $scriptProperties['path'];
-$path_url = $scriptProperties['path_url'];
-$field = $scriptProperties['field'];
+$path = $scriptProperties['photo_path'];
+$path_url = $scriptProperties['photo_path_url'];
+$field = $scriptProperties['photo_field'];
 $fieldValue = $hook->getValue($field);
 
-if (empty($path) || empty($path_url) || empty($field)) {
-    $configError = 'path, path_url and/or field properties not defined (all are required).';
-    //$hook->addError('message', $configError);
-    $modx->log(modX::LOG_LEVEL_ERROR, $configError);
+//$field_error = '';
+$configError = ' field property not defined.';
+if (empty($path)) {
+    $field_error = 'photo_path';
+    $modx->log(modX::LOG_LEVEL_ERROR, $field_error.$configError);
+    return false;
+}
+if (empty($path_url)) {
+    $field_error = 'photo_path_url';
+    $modx->log(modX::LOG_LEVEL_ERROR, $field_error.$configError);
+    return false;
+}
+if (empty($field)) {
+    $field_error = 'photo_field';
+    $modx->log(modX::LOG_LEVEL_ERROR, $field_error.$configError);
     return false;
 }
 
@@ -29,14 +40,14 @@ if(!empty($fieldValue['name'])) {
      * [name], [type], [tmp_name], [error], [size]
      */
     // @TODO: define some allowed file extensions
-    $ext = pathinfo($fieldValue['name'], PATHINFO_EXTENSION);
-    $filename = $user->get('username').'-profile.'.$ext;
+    $ext = '.'.pathinfo($fieldValue['name'], PATHINFO_EXTENSION);
+    $filename = $scriptProperties['photo_filename'] ? $scriptProperties['photo_filename'].$ext : $user->get('username').'-profile'.$ext;
     // @TODO: make sure the filename is unique & "web URL" compatible
     $full_path = $path.$filename;
     $full_url = $path_url.$filename;
 
     if (!move_uploaded_file($fieldValue['tmp_name'], $full_path)) {
-        //$hook->addError($picture, 'File not uploaded');
+        //$hook->addError($field, 'File not uploaded');
         return false;
     }
 
